c983720b4313297271eec64bde836587f89906f7
Dwin <E.R.Steinmetz@gmail.com>
2015-07-22 21:35:29 -0500

    wfh commit

--- public/js/application.js
+++ public/js/application.js
@@ -2,48 +2,94 @@
 var userTypes = {
   newUser: "<input id='hidden-new' class='user-login-type' type='hidden' name='user[type]' value='new'>",
   loginUser: "<input id='hidden-login' class='user-login-type' type='hidden' name='user[type]' value='login'>"
-  };
+};
 
-  function loginToggle(){
-    $(".user-button").toggle();
-    $("#user-form").toggle();
-    clearForm();
-  };
+function loginToggle(){
+  $(".user-button").toggle();
+  $("#user-form").toggle();
+  clearForm();
+};
 
-  function clearForm(){
-    // select the form with jquery, then grab the DOMElement
-    // then use its reset method
-    $("#user-form")[0].reset()
-  };
+function clearForm(){
+  // select the form with jquery, then grab the DOMElement
+  // then use its reset method
+  $("#user-form")[0].reset()
+};
 
-  function userManager() {
-    this.users = []
-  };
+function userManager() {
+  this.users = []
+};
 
-  userManager.prototype.prepareUserSession = function(){
-    // this does not seem to be working the way I expect
-    //
-    $("#race-character-promp").toggle();
-    return $("body").on('keydown', function(press){
-      if(!press.metaKey){
-        press.preventDefault;
-      };
-      $("#race-character-promp").toggle();
-      return press.keyCode
-    });
-  };
+userManager.prototype.attachToUserButtons = function(){
+  this.attachToLoginUserButton();
+  this.attachToNewUserButton();
+  this.attachToSubmitUserButton();
+};
 
-  userManager.prototype.fetchUser =function (formData) {
-    return $.ajax({
-      url: url,
-      data: formData,
-      dataType: 'json',
-      method: 'post'
-    });
-  };
+userManager.prototype.attachToLoginUserButton = function(){
+  $("#login-button").on("click", function(e){
+    e.preventDefault();
+    loginToggle();
+    $("#user-form").append(userTypes.loginUser)
+  });
+};
+
+userManager.prototype.attachToNewUserButton = function(){
+  $("#new-user-button").on("click", function(e){
+    e.preventDefault();
+    loginToggle();
+    $("#user-form").append(userTypes.newUser)
+  });
+};
+
+userManager.prototype.attachToSubmitUserButton = function(){
+  $("#user-form").on("submit", function(e){
+    e.preventDefault();
+    loginToggle();
+    $(".user-login-type").remove();
+
+    var speedRacer = new Racer();
+    speedRacer.initialize($("#user-form").serialize());
+    //will the "this" below go to kathulu or #user-form..?
+    this.users.push(speedRacer);
+  });
+};
+
+function Racer() {};
+
+Racer.prototype.initialiaze = function(userParams){
+  this.username = userParams.username; // not sure if this works
+};
+
+
+
+                    // under construction
+                    userManager.prototype.prepareUserSession = function(){
+                      // this does not seem to be working the way I expect
+                      //
+                      $("#race-character-promp").toggle();
+                      return $("body").on('keydown', function(press){
+                        if(!press.metaKey){
+                          press.preventDefault;
+                        };
+                        $("#race-character-promp").toggle();
+                        return press.keyCode
+                      });
+                    };
+
+                    userManager.prototype.fetchUser =function (formData) {
+                      return $.ajax({
+                        url: url,
+                        data: formData,
+                        dataType: 'json',
+                        method: 'post'
+                      });
+                    };
 
   // userManager needs...
   // take login data from the form
+    // to do this, I need to set up an event listener for the form ...in an initialize method?
+    // 
   // validate / create user in db
   // create & store a js-user obj
 
@@ -59,27 +105,12 @@ var userTypes = {
 
 $(document).ready(function() {
 
-  $("#login-button").on("click", function(e){
-    e.preventDefault();
-    loginToggle();
-    $("#user-form").append(userTypes.loginUser)
-  });
+  var kathulu = new userManager();
+  kathulu.attachToUserButtons();
+  
 
-  $("#new-user-button").on("click", function(e){
-    e.preventDefault();
-    loginToggle();
-    $("#user-form").append(userTypes.newUser)
-  });
 
-  $("#user-form").on("submit", function(e){
-    e.preventDefault();
-    loginToggle();
-    $(".user-login-type").remove();
 
-    console.log($("#user-form").serialize())
-  });
-
-// user%5Busername%5D=u&user%5Bpassword%5D=p
 
 
  }); // closes ready
