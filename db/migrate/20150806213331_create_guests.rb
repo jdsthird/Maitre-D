bf6cc3ff2e8fdbf4f3a3ed24cf6aa81fc94790f1
caonayemi <caonayemi@gmail.com>
2015-08-06 17:54:14 -0500

    Add references and seat number to guests table

--- db/migrate/20150806213331_create_guests.rb
+++ db/migrate/20150806213331_create_guests.rb
@@ -1,8 +1,11 @@
 class CreateGuests < ActiveRecord::Migration
   def change
     create_table :guests do |t|
-      t.string   :first_name
-      t.string   :last_name
+      t.string     :first_name
+      t.string     :last_name
+      t.references :event
+      t.references :table
+      t.integer    :seat_number
 
       t.timestamps null: false
     end
