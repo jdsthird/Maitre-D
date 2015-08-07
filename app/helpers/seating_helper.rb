f350f3752f0bfed006eb3949a0767a74522db85b
John Sternberg <jdsthird@gmail.com>
2015-08-07 15:29:51 -0500

    pause for afternoon check in

---
+++ app/helpers/seating_helper.rb
@@ -0,0 +1,35 @@
+module SeatingHelper
+
+  # aggregate guests into collections
+  def group_guests(guests)
+    
+  end
+
+  # generate permutations of collections
+  def check_permutations(collections, tables)
+    collections.permutation.each do |permutation|
+      guests = permutation.flatten
+      tables = seat_guests(tables, guests)
+      if tables_valid?(tables)
+        return tables
+      else
+        false
+      end
+    end
+  end
+
+  # map guests to tables based upon generated permutations
+  def seat_guests(tables, guests)
+    tables.map do |table|
+      table.number_of_seats.times do
+        table.guests << guests.shift
+      end
+      table
+    end
+    tables
+  end
+  # run a method on each table to ensure it meets the requirements
+  def tables_valid?(tables)
+    tables.all? { |table| table.valid? }
+  end
+end
---
+++ spec/helpers/seating_helper_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe SeatingHelper, type: :helper do
+  describe "#seat_guests" do
+    xit "returns an array of tables" do
+
+    end
+
+    xit "correctly maps guests to tables" do
+      
+    end
+  end
+end
