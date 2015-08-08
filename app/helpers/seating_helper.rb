ee6341bf60627750c583dfb5c29b9a75f095baac
John Sternberg <jdsthird@gmail.com>
2015-08-08 18:12:43 -0500

    update seating helper and specs

--- app/helpers/seating_helper.rb
+++ app/helpers/seating_helper.rb
@@ -1,33 +1,53 @@
 module SeatingHelper
 
   # aggregate guests into collections
-  def group_guests(guests)
-    collections = guests.map do |guest|
-      guest.id | guest.pairs.map(&:id)
-    end
-    collections.uniq
-  end
+  # def group_guests(guests)
+  #   collections = []
+  #   guests.sort! { |guest| guest.pairs.length }
+  #   no_change = false
+  #   until guests.empty?
+  #     collections << [guests.shift]
+  #     until no_change
+  #       no_change = true
+  #       collections.map! do |collection|
+  #         expanded_collection = collection.map do |guest|
+  #           collection += guest.pairs
+  #           collection.uniq
+  #         end
+  #         if expanded_collection == collection
+  #           collection
+  #         else
+  #           no_change = false
+  #           expanded_collection
+  #         end
+  #       end
+  #     end
+  #     guests -= collections.flatten
+  #   end
+  #   collections
+  # end
 
   # generate permutations of collections
-  def check_permutations(collections, tables)
-    collections.permutation.each do |permutation|
-      guests = permutation.flatten
+  def check_permutations(guests, tables)
+    guests.permutation.each do |permutation|
+      # guests = permutation.flatten
       tables = seat_guests(tables, guests)
       if tables_valid?(tables)
         return tables
-      else
-        false
       end
     end
+    false
   end
 
   # map guests to tables based upon generated permutations
   def seat_guests(tables, guests)
-    tables.map do |table|
-      table.number_of_seats.times do
-        table.guests << guests.shift
-      end
-      table
+    number_of_seats = tables.first.number_of_seats
+    unless tables.all? { |table| table.number_of_seats == number_of_seats }
+      raise ArgumentError.new("all tables must have an equal number of seats")
+    end
+    groups = guests.each_slice(number_of_seats)
+    tables.each do |table|
+      table.guests << groups.shift
     end
     tables
   end
--- spec/helpers/seating_helper_spec.rb
+++ spec/helpers/seating_helper_spec.rb
@@ -1,6 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe SeatingHelper, type: :helper do
+
+  let(:host) { User.create!(first_name: "a", last_name: "b", email: "a@b.c", username: "d", password_hash: BCrypt::Password.create("1"))}
+  let(:event) { Event.create!(name: "p", zip_code: "12345", state: "IL", address_line_one: "123 Elm St.", begins_at: Time.now, host: host) }
+  let(:guest) { Guest.create!(first_name: "sam", event: event) }
+  let(:guests) { Array.new(5) { Guest.create!(first_name: "guest", event: event) } }
+  let(:table) { Table.create!(number_of_seats: 5, event: event) }
+  let(:tables) { Array.new(2) { Table.create!(number_of_seats: 5, event: event) } }
+
+  describe "#group_guests" do
+    xit "accepts an array of guests as its argument" do
+      expect{ group_guests(guests) }.to_not raise_error
+    end
+
+    xit "returns an array of arrays" do
+      expect(group_guests(guests)).to satisfy { |group| group.class == Array }
+    end
+
+    xit "groups pairs together" do
+      guests.each { |g| guest.pairs << g }
+      p output = group_guests(guests << guest)
+      expect(output.length).to eq 1
+    end
+  end
+
   describe "#seat_guests" do
     xit "returns an array of tables" do
 
@@ -8,6 +32,20 @@ RSpec.describe SeatingHelper, type: :helper do
 
     xit "correctly maps guests to tables" do
       
+    end
+  end
+
+  describe "#tables_valid?" do
+    it "returns false if any table is not valid" do
+      guests.first.pairs << guest
+      tables[0].guests += guests
+      tables[1].guests << guest
+      expect(tables_valid?(tables)).to be false
+    end
+
+    it "returns true when all tables are valid" do
+      table.stub(:valid_seating?).and_return true
+      expect(tables_valid?(tables)).to be true
     end
   end
 end
