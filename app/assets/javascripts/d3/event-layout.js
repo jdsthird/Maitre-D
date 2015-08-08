58b07ab751e6245a5fa6bdb7f43747164fd89381
caonayemi <caonayemi@gmail.com>
2015-08-08 17:01:48 -0500

    Add d3 library to vendors assets

--- app/assets/javascripts/d3/event-layout.js
+++ app/assets/javascripts/d3/event-layout.js
@@ -1,13 +1,22 @@
-var test = d3.select("body").append("div")
+var test = d3.select("h1")
   .style("background-color", "blue")
 
 
-// var svg = d3.select('#table-1');
-// var originX = 75;
-// var originY = 75;
-// var tableRadius = 40;
+var svg = d3.select('.tables').append("svg:g")
+  .attr("width", 150)
+  .attr("height", 150);
 
-// var table = svg.append("circle").attr({
+var originX = 75;
+var originY = 75;
+var tableRadius = 40;
+
+var table = svg.append("svg:circle")
+  .attr("cx", originX)
+  .attr("cy", originY)
+  .attr("r", tableRadius)
+  .style("fill", "#595AB7");
+
+// ({
 //   cx: originX,
 //   cy: originY,
 //   r: tableRadius,
