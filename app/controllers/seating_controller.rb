6c279a93129e301cbcbfae936fc18aac10000f9a
John Sternberg <jdsthird@gmail.com>
2015-08-12 14:48:14 -0500

    add new route to seat guests.

--- app/controllers/seating_controller.rb
+++ app/controllers/seating_controller.rb
@@ -1,9 +1,18 @@
 class SeatingController < ApplicationController
   def seat_guests
     if current_event == Event.find_by_id(params[:id])
+      current_event
       tables = current_event.tables
       guests = current_event.guests
-      check_sufficient_seating(tables, guests)
+      unless check_sufficient_seating(tables, guests)
+        event.errors.add(:seating, "must be sufficient for the number of guests.")
+        redirect_to event
+      end
+      full_tables = make_seating_chart(guests, tables)
+      if full_tables
+        full_tables.each(&:save)
+      end
+      redirect_to current_event
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
 
-  get 'seating/event/:id' => 'seating#seat_guests'
+  get 'seating/events/:id' => 'seating#seat_guests'
 
 
   resources :events
