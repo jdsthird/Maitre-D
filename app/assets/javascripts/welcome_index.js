7ced9c78b7f35be9c1659537d0387e668f80ae27
caonayemi <caonayemi@gmail.com>
2015-08-12 18:08:52 -0500

    Add ajax for welcome page

---
+++ app/assets/javascripts/welcome_index.js
@@ -0,0 +1,68 @@
+$(document).ready(function() {
+  $("#login").on("click", function(event) {
+    event.preventDefault();
+    var request = $.ajax({
+      url: $(this).find("a").attr("href")
+    });
+
+    request.done(function(response) {
+      $(".button-container").remove();
+      $(".form").html(response);
+    });
+  });
+
+  $(".form").on("click", "#link-to-signup", function(event) {
+    event.preventDefault();
+    var request = $.ajax({
+      url: $(this).attr("href")
+    });
+
+    request.done(function(response) {
+      $("form").remove();
+      $(".form").html(response);
+    });
+  });
+
+  $(".form").on("submit", "form", function(event) {
+    event.preventDefault();
+
+    var request = $.ajax({
+      url: "/login",
+      method: "POST",
+      data: $(this).serialize(),
+      dataType: "json"
+    });
+
+    request.success(function() {
+
+    });
+
+    request.fail(function(response) {
+      $(".form").html(response.responseText);
+    });
+  });
+
+  $("#signup").on("click", function(event) {
+    event.preventDefault();
+    var request = $.ajax({
+      url: $(this).find("a").attr("href")
+    });
+
+    request.done(function(response) {
+      $(".button-container").remove();
+      $(".form").html(response);
+    });
+  });
+
+  $(".form").on("click", "#link-to-login", function(event) {
+    event.preventDefault();
+    var request = $.ajax({
+      url: $(this).attr("href")
+    });
+
+    request.done(function(response) {
+      $("form").remove();
+      $(".form").html(response);
+    });
+  });
+});
