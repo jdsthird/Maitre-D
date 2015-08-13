388f5ee0a4dd5d015a08be10c16b7ddec3dd6ff0
John Sternberg <jdsthird@gmail.com>
2015-08-13 08:57:58 -0500

    remove code rendered unnecessary by changes to event#show

--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,6 @@ Rails.application.routes.draw do
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
 
-  get 'seating/events/:id' => 'seating#seat_guests'
-
-
   resources :events
 
   resources :pairings, only: [:index, :create, :destroy]
