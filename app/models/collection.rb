d6d70deb14aa5492782fbaa5c623061e7d3d5545
John Sternberg <jdsthird@gmail.com>
2015-09-13 23:24:17 -0500

    add merge method to collection model

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,3 +1,12 @@
 class Collection < ActiveRecord::Base
   has_many :guests
+
+  def merge(collection)
+    collection.guests.map do |guest|
+      self.guests << guest
+    end
+    self.num_guests += collection.num_guests
+    self.save!
+    collection.destroy
+  end
 end
