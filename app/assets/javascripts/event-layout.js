89d5b609a3f0d79d0dbf3503317fd729199a69b3
thunderenlight <thunderenlight@gmail.com>
2015-09-15 18:51:04 -0500

    Dropping and dragging with poor boundaries

--- app/assets/javascripts/event-layout.js
+++ app/assets/javascripts/event-layout.js
@@ -2,10 +2,11 @@
 
 
 var guestList = new Guests();
-guestList.fetch({success: function(){
+  guestList.fetch({success: function(){
 
-  guestList.eventTableIds().forEach(function(table_id, i){
+  var data = guestList.eventTableIds().forEach(function(table_id, i){
 
+    console.log(data);
     var group = d3.select('#table-' + (i+1) ).append('g')
 
     var originX = 75;
@@ -13,9 +14,9 @@ guestList.fetch({success: function(){
     var tableRadius = 40;
     var chairsRadius = 55;
     var chairWidth = 15;
-
     var chairOriginX = originX + (chairsRadius * Math.sin(0));
     var chairOriginY = originY - (chairsRadius * Math.cos(0));
+   
 
     var table = group.append("circle").attr({
       cx: originX,
@@ -26,6 +27,16 @@ guestList.fetch({success: function(){
 
     var tableOne = guestList.tableSelector(table_id);
     var rotationFactor = Math.floor(360 / tableOne.length);
+    var drag = d3.behavior.drag()
+    .on("drag", dragmove);
+
+    function dragmove(d) {
+      var x = d3.event.x;
+      var y = d3.event.y;
+      d3.select(this).attr("transform", "translate(" + x + "," + y + ")");
+    }
+
+
 
     for(var ii =0; ii < tableOne.length; ii++){
       var currentChair = group.append("rect").attr({
@@ -41,7 +52,9 @@ guestList.fetch({success: function(){
         id: tableOne[ii].id
         
       });
+
       currentChair.attr("transform", "rotate(" + (ii*rotationFactor) + ", " + originX + ", " + originY + ")");
+      currentChair.call(drag);
     }
   });
   
